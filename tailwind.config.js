3e5c0093de518ae401da5caa3b5e7422888a20b1
Esteban <69709996+Lefty-Glasses@users.noreply.github.com>
2024-06-14 19:15:34 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        sloke: ["Sloke", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
